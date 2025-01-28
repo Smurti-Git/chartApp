4ec21442ed4eb591ed3ac9f31953f84fff8b19fe
Smurti Swain <smurtiswain1999@gmail.com>
2025-01-28 19:20:48 +0530

    do ready for deploy

--- server.js
+++ server.js
@@ -1,17 +1,33 @@
-const io = require('socket.io')(3000)
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
 
-const users = {}
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static('public')); // Serve static files
+
+const users = {};
 
 io.on('connection', socket => {
   socket.on('new-user', name => {
-    users[socket.id] = name
-    socket.broadcast.emit('user-connected', name)
-  })
+    users[socket.id] = name;
+    socket.broadcast.emit('user-connected', name);
+  });
   socket.on('send-chat-message', message => {
-    socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
-  })
+    socket.broadcast.emit('chat-message', {
+      message: message,
+      name: users[socket.id],
+    });
+  });
   socket.on('disconnect', () => {
-    socket.broadcast.emit('user-disconnected', users[socket.id])
-    delete users[socket.id]
-  })
-})
+    socket.broadcast.emit('user-disconnected', users[socket.id]);
+    delete users[socket.id];
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
