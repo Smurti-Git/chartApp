7a87c4bcc8b0e71cb256a2f9c4a0bc0805cb272c
Smurti Swain <smurtiswain1999@gmail.com>
2025-01-28 20:39:23 +0530

    fix issue

--- server.js
+++ server.js
@@ -3,24 +3,26 @@ const http = require('http');
 const { Server } = require('socket.io');
 
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
+const server = http.createServer(app); // Create HTTP server
+const io = new Server(server); // Pass HTTP server to Socket.IO
 
-app.use(express.static('public')); // Serve static files
+// Serve static files
+app.use(express.static('public'));
 
 const users = {};
 
 io.on('connection', socket => {
+  console.log('A user connected');
+  
   socket.on('new-user', name => {
     users[socket.id] = name;
     socket.broadcast.emit('user-connected', name);
   });
+
   socket.on('send-chat-message', message => {
-    socket.broadcast.emit('chat-message', {
-      message: message,
-      name: users[socket.id],
-    });
+    socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] });
   });
+
   socket.on('disconnect', () => {
     socket.broadcast.emit('user-disconnected', users[socket.id]);
     delete users[socket.id];
